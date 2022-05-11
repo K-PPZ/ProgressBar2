de11a1572ffdc633c53013fd95279d07c8e66a44
KorenPapazian <kpapazian@edenschool.fr>
2022-05-11 11:51:38 +0200

    Css and Js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-let progression = document.getElementById("Progress");
-let interval = setInterval(move, 1000);
+let width = 0;
 
 function move() {
-    
+    let progressing = document.getElementById("Progress");
+    let interval = setInterval(progression, 100);
+
+    function progression () {
+        width ++;
+        if (width > 100) {
+            clearInterval(interval);
+        } else {
+            progressing.style.width = width + '%';
+        }
+    }
 }
