4c9b953853fbeccd6fada6a663078e7d0daced0f
KorenPapazian <kpapazian@edenschool.fr>
2022-05-11 14:10:55 +0200

    js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let width = 0;
 
 function move() {
     let progressing = document.getElementById("Progress");
-    let interval = setInterval(progression, 100);
+    let interval = setInterval(progression, Math.random() * (10 - 100) + 100);
 
     function progression () {
         width ++;
